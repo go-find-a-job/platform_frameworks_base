5deb091c93fc88ce3df54d69b63a6174e34881d8
Pranav Vashi <neobuddy89@gmail.com>
2018-05-05 23:43:15 +0300

    QS: Add CPUInfo toggle tile
    Original commit by @mydongistiny
    Heavily modified by @neobuddy89
    to adapt to slashify
    
    Change-Id: If811c93a476ec2ddece1f15103b3e4bcdfc650c1
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -35,6 +35,7 @@ import com.android.systemui.qs.tiles.CaffeineTile;
 import com.android.systemui.qs.tiles.CastTile;
 import com.android.systemui.qs.tiles.CellularTile;
 import com.android.systemui.qs.tiles.ColorInversionTile;
+import com.android.systemui.qs.tiles.CPUInfoTile;
 import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
@@ -204,6 +205,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new MusicTile(mHost);
             case "hwkeys":
                 return mHWKeysTileProvider.get();
+	        case "cpuinfo":
+                return new CPUInfoTile(mHost);
         }
 
         // Intent tiles.
